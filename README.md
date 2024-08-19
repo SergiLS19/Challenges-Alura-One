9b6006b629681f6501ea6be2388b2597fb848d3e
SergiLS19 <sergi.lsantti.9@gmail.com>
2024-08-19 03:47:02 -0500

    Done

---
+++ README.md
@@ -0,0 +1,53 @@
+<h1>Challenge: Encriptador de Texto by Alura LATAM</h1>
+
+Este proyecto es un encriptador de texto sencillo que transforma las vocales de un texto ingresado. Es ideal para practicar conceptos básicos de JavaScript y manipulación del DOM.
+
+## Autor
+
+**Sergio L. Santti ⚡**
+
+
+
+
+
+
+## Descripción
+
+El encriptador funciona reemplazando cada vocal en el texto de entrada con una secuencia específica. Además, incluye un botón para desencriptar el texto transformado, devolviéndolo a su forma original.
+
+## Características
+
+- **Encriptación:** Transforma el texto ingresado según las reglas definidas.
+- **Desencriptación:** Revierte el texto encriptado a su estado original.
+- **Copiar al portapapeles:** Permite copiar el texto encriptado o desencriptado para usarlo en otros lugares.
+- **Interfaz de usuario:** Diseño sencillo y funcional, con efectos visuales que mejoran la experiencia de usuario.
+
+## Tecnologías utilizadas
+
+- **HTML:** Estructura del contenido.
+- **CSS:** Estilos y efectos visuales, como la animación de botones.
+- **JavaScript:** Lógica de encriptación y desencriptación, manipulación del DOM.
+
+## Uso
+
+1. Clona este repositorio en tu máquina local:
+    ```bash
+    git clone https://github.com/tu-usuario/encriptador-de-texto.git
+    ```
+ o usando este enlace:    
+2. Abre el archivo `index.html` en tu navegador web.
+3. Escribe o pega el texto que deseas encriptar en el campo de entrada.
+4. Haz clic en el botón "Encriptar" para transformar el texto.
+5. Utiliza el botón "Desencriptar" para revertir el texto encriptado.
+6. Copia el resultado usando el botón "Copiar".
+
+## Contribuciones
+
+¡Las contribuciones son bienvenidas! Si tienes ideas para mejorar este proyecto, no dudes en hacer un fork del repositorio y enviar un pull request.
+
+  
+## Redes Sociales
+<li> GitHub:https://github.com/SergiLS19 </li>
+<li> LinkedIn: www.linkedin.com/in/sergio-lama-santti-8779a125b </li>
+<li> Instagram: lsantti.95 </li>
+</ul> 
---
+++ scrpt/script.js
@@ -0,0 +1,82 @@
+let btnEncript = document.querySelector(".btn_encriptar");
+let btnDesencript = document.querySelector(".btn_desencriptar");
+let btnCopiar = document.querySelector(".btn_copiar");
+let txtEncript = document.querySelector(".texto");
+let warning = document.querySelector(".aviso");
+let result = document.querySelector(".resultado");
+let muñeco = document.querySelector(".img-muñeco");
+let mensajeUno = document.querySelector(".mensaje_uno");
+let mensajeDos = document.querySelector(".mensaje_dos");
+
+function encriptar(){
+  let texto=txtEncript.value;
+  let txt=texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, "");
+  if (texto==""){
+    swal("¡Hey!","Necesito un texto para poder alterar", "warning");
+
+  }
+  else if(texto !== txt){
+    swal("¡Hey!","No deben haber acentos ni caracteres distintos", "warning");
+  }
+  else if(texto!==texto.toLowerCase()){
+    swal("¡Hey!","Solo se permiten letras minúsculas", "warning");   
+  }
+
+  else{
+    let textoEncriptado =texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai") 
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+    result.innerHTML= textoEncriptado
+    btnCopiar.style.visibility="inherit";
+    result.style.visibility="inherit";
+    muñeco.src="./assets/CANDADO_CERRADO_FONDO.png"
+    mensajeUno.textContent="¡Tu mensaje ha sido encriptado!"
+    mensajeDos.textContent="";
+  }
+
+}
+
+function desencriptar(){
+  let texto=txtEncript.value;
+  let txt=texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, "");
+  if (texto==""){
+    swal("¡Hey!","Necesito un texto para poder alterar", "info");
+
+  }
+  else if(texto !== txt){
+    swal("¡Hey!","No deben haber acentos ni caracteres distintos", "info");
+  }
+  else if(texto!==texto.toLowerCase()){
+    swal("¡Hey!","Solo se permiten letras minúsculas", "info");   
+  }
+
+  else{
+    let textoEncriptado =texto
+    .replace(/enter/gi,"e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi,   "a") 
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+    result.innerHTML= textoEncriptado
+    btnCopiar.style.visibility="inherit";
+    result.style.visibility="inherit";
+    muñeco.src="./assets/CANDADO_ABIERTO_FONDO.png"
+    mensajeUno.textContent="¡Tu mensaje ha sido desencriptado!"
+    mensajeDos.textContent="";
+  }
+
+}
+function copiar(){
+
+  let copiar = result.textContent;
+  navigator.clipboard.writeText(copiar)
+        .then(() => {
+        swal("Copiado", "El texto fue copiado a su portapapeles", "success");
+        })
+        .catch(err => {
+        swal("Oh...","Hubo un error al copiar el texto", "error");
+        });
+}
